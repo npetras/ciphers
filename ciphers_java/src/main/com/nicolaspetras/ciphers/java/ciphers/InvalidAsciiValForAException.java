728d9d1f2b166f03e5db99ac922d19d1b327d01b
Nicolas <nicolaspetras@gmail.com>
2020-01-01 22:45:34 +0000

    Created custom exception classes
    To be thrown by methods in Caesar and Vigenere, in error cases

---
+++ ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/InvalidAsciiValForAException.java
@@ -0,0 +1,8 @@
+package com.nicolaspetras.ciphers.java.ciphers;
+
+/**
+ * An exception that is thrown when an invalid ASCII value for A is provided - a value not 65 or 97
+ */
+public class InvalidAsciiValForAException extends Exception
+{
+}
---
+++ ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/UnexpectedNonAlphaCharException.java
@@ -0,0 +1,8 @@
+package com.nicolaspetras.ciphers.java.ciphers;
+
+/**
+ * An exception class that indicates that a non-alphabetical character was evaluated at an unexpected point.
+ */
+public class UnexpectedNonAlphaCharException extends Exception
+{
+}
