52b7f37ac5f4c35fc7284d14734d0d07a94fa0bc
Nicolas <nicolaspetras@gmail.com>
2020-01-02 16:21:55 +0000

    Made modifications to the Caesar tests and Tests class
    Commented out code that no longer compiled in Tests

--- ciphers_java/src/test/com/nicolaspetras/ciphers/java/CaesarTest.java
+++ ciphers_java/src/test/com/nicolaspetras/ciphers/java/CaesarTest.java
@@ -1,14 +1,43 @@
 package com.nicolaspetras.ciphers.java;
 
-import org.junit.jupiter.params.*;
-import org.junit.jupiter.params.provider.ValueSource;
+import com.nicolaspetras.ciphers.java.ciphers.Caesar;
+import com.nicolaspetras.ciphers.java.ciphers.InvalidAsciiValForAException;
+import com.nicolaspetras.ciphers.java.ciphers.UnexpectedNonAlphaCharException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+/**
+ * Test class for the Caesar class.
+ * @see Caesar
+ */
 public class CaesarTest
 {
+    /**
+     * Test for the encipher method.
+     * <p>Tests with a plaintext that contains alphabetical, space and special characters. The same plaintext is used
+     * for each test, but different keys are applied that plaintext - both negative and positive, including keys
+     * greater than 25, which the Caesar should be able to handle if required.
+     *
+     * @param key The Caesar key to be used
+     * @param expected The expected ciphertext to be output by the encipher method
+     * @throws UnexpectedNonAlphaCharException
+     */
     @ParameterizedTest
-    @ValueSource(strings = { "racecar", "radar", "able was I ere I saw elba" })
-    public void testEncipher(int key, String expected) {
+    @CsvSource({
+            "3, Khoor Zruog! 123&*%",
+            "-3, Ebiil Tloia! 123&*%",
+            "45, Axeeh Phkew! 123&*%",
+            "-45, Olssv Dvysk! 123&*%"
+    })
+    public void testEncipher(int key, String expected) throws UnexpectedNonAlphaCharException,
+            InvalidAsciiValForAException
+    {
+        Caesar caesar = new Caesar(key);
+        String plaintext = "Hello World! 123&*%";
+        String actual = caesar.encipher(plaintext);
 
+        Assertions.assertEquals(expected, actual);
     }
 
 }
