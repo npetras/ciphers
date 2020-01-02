00021ca360cde62a6803d4e143309a97bfab7ceb
Nicolas <nicolaspetras@gmail.com>
2020-01-02 16:21:07 +0000

    Added unit tests for Vigenere

---
+++ ciphers_java/src/test/com/nicolaspetras/ciphers/java/VigenereTest.java
@@ -0,0 +1,34 @@
+package com.nicolaspetras.ciphers.java;
+
+import com.nicolaspetras.ciphers.java.ciphers.InvalidAsciiValForAException;
+import com.nicolaspetras.ciphers.java.ciphers.UnexpectedNonAlphaCharException;
+import com.nicolaspetras.ciphers.java.ciphers.Vigenere;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Test class for Vigenere.
+ *
+ * @see Vigenere
+ */
+public class VigenereTest
+{
+
+    @ParameterizedTest
+    @CsvSource({
+            "abc, Hfnlp Yosnd! 123&*%",
+            "Aberystwyth, Hfpcm Ohnjw! 123&*%",
+            "HELLOSOS, Oiwwc Ocjsh! 123&*%"
+    })
+    public void testEncipher(String key, String expected) throws UnexpectedNonAlphaCharException,
+            InvalidAsciiValForAException
+    {
+        Vigenere vigenere = new Vigenere(key);
+        String plaintext = "Hello World! 123&*%";
+        String actual = vigenere.encipher(plaintext);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
