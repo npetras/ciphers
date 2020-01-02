9b9a9d65c50248ec0e221a010008a4c1cb07679c
Nicolas <nicolaspetras@gmail.com>
2020-01-02 16:20:45 +0000

    Added a new exception class

---
+++ ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/InvalidVigenereKeyException.java
@@ -0,0 +1,8 @@
+package com.nicolaspetras.ciphers.java.ciphers;
+
+/**
+ * Exception that is thrown when an invalid Vigenere key is provided.
+ */
+public class InvalidVigenereKeyException extends Exception
+{
+}
