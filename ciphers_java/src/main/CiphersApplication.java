ad671392c1da34578aae058cbe82aa01e40b2352
Nicolas <nicolaspetras@gmail.com>
2020-03-29 17:35:35 +0100

    Added print statements and pseudocode comments
    To the main application method

--- ciphers_java/src/main/CiphersApplication.java
+++ ciphers_java/src/main/CiphersApplication.java
@@ -1,3 +1,22 @@
 public class CiphersApplication
 {
+    public static void main(String[] args) {
+
+        String plainText;
+        String cipherType;
+
+        System.out.println("----------------------------- Ciphers -----------------------------");
+        System.out.println("This application allows you to encipher any piece of text ");
+        System.out.println("(plain text), using one of two ciphers: Caesar or Vigenere");
+        System.out.println("into cipher text\n");
+        System.out.println("Please provide the plain text you want to encipher: ");
+        // take users input
+        System.out.println("Which cipher would you like to use?");
+        System.out.println("Type 'c' for the Caesar Cipher, and 'v' for the Vigenere cipher: ");
+        // take user input
+        // trim input, and toLowerCase
+
+        // encipher the plain text based on the input
+        // print out the cipher text
+    }
 }
