6e8f3bad6ebe3f5739a9dbfcea994e6ed9086ff9
Nicolas <nicolaspetras@gmail.com>
2020-03-29 18:25:33 +0100

    feat: Added code to get the type of cipher to use from the user
    Also started to work to the switch to handle the two different cipher types

--- ciphers_java/src/main/CiphersApplication.java
+++ ciphers_java/src/main/CiphersApplication.java
@@ -1,22 +1,71 @@
+import com.nicolaspetras.ciphers.java.ciphers.Caesar;
+import com.nicolaspetras.ciphers.java.ciphers.UnknownCipherType;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
 public class CiphersApplication
 {
-    public static void main(String[] args) {
+    private static Scanner scan = new Scanner(System.in);
+    private static Caesar caesar;
 
-        String plainText;
-        String cipherType;
 
+    /**
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws UnknownCipherType
+    {
+        printWelcomeMessage();
+        String plainText = getStringInputFromUser("Please provide the plain text you want to encipher: ");
+        String cipherType = getStringInputFromUser("Which cipher would you like to use? \n" +
+                "Type 'c' for the Caesar Cipher, and 'v' for the Vigenere cipher: ");
+
+        cipherType = cipherType.trim();
+        cipherType = cipherType.toLowerCase();
+
+        String cipherText;
+
+        switch (cipherType) {
+            case "c":
+                // get Caesar cipher key
+                // encipher plain text with that cipher key
+                break;
+            case "v":
+                // get Vigenere cipher key
+                // encipher plain text using that cipher key
+                break;
+            default:
+                throw new UnknownCipherType("An Unknown Cipher type" + cipherType + "was provided");
+        }
+
+        // encipher the plain text based on the input
+        // print out the cipher text
+    }
+
+    /**
+     * Prints a message prompt to the user and then receives String input from the user.
+     *
+     * @param message The message to be printed to the user, as a prompt for the data they should provide.
+     * @return The String input received from the user.
+     */
+    private static String getStringInputFromUser(String message)
+    {
+        System.out.println(message);
+        return scan.nextLine();
+    }
+
+    /**
+     * Prints the welcome message for the application to the user.
+     */
+    private static void printWelcomeMessage()
+    {
         System.out.println("----------------------------- Ciphers -----------------------------");
         System.out.println("This application allows you to encipher any piece of text ");
         System.out.println("(plain text), using one of two ciphers: Caesar or Vigenere");
         System.out.println("into cipher text\n");
-        System.out.println("Please provide the plain text you want to encipher: ");
-        // take users input
-        System.out.println("Which cipher would you like to use?");
-        System.out.println("Type 'c' for the Caesar Cipher, and 'v' for the Vigenere cipher: ");
-        // take user input
-        // trim input, and toLowerCase
-
-        // encipher the plain text based on the input
-        // print out the cipher text
     }
 }
---
+++ ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/UnknownCipherType.java
@@ -0,0 +1,8 @@
+package com.nicolaspetras.ciphers.java.ciphers;
+
+public class UnknownCipherType extends Exception
+{
+    public UnknownCipherType(String message) {
+        super(message);
+    }
+}
