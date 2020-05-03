7d2b6dad347dc4eec5473cf74d9fa65d0eff926c
Nicolas <nicolaspetras@gmail.com>
2020-05-03 16:08:55 +0100

    Update Exception to encompass all ciphers

--- ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/InvalidCipherKeyException.java
+++ ciphers_java/src/main/com/nicolaspetras/ciphers/java/ciphers/InvalidCipherKeyException.java
@@ -3,6 +3,6 @@ package com.nicolaspetras.ciphers.java.ciphers;
 /**
  * Exception that is thrown when an invalid Vigenere key is provided.
  */
-public class InvalidVigenereKeyException extends Exception
+public class InvalidCipherKeyException extends Exception
 {
 }
